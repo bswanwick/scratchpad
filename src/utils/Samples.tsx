40f972dccf9a87666df160e365cd7d3e74631f50
blaise <blaise@advancedkiosks.com>
2019-12-27 17:04:40 -0500

    Samples don't play

--- src/utils/Samples.tsx
+++ src/utils/Samples.tsx
@@ -1,12 +1,29 @@
-interface AllSamples {
-  [key: string]: any;
+import { Howl } from "howler";
+
+// Get all files from the /samples directory
+const context = require.context("../samples", true, /\.(wav|mp3)$/);
+
+export interface SamplesByKey {
+  [fileName: string]: Sample;
 }
-var context = require.context("./samples", true, /\.(wav|mp3)$/);
-debugger;
-var allSamples: AllSamples = {};
+export interface Sample {
+  filename: string;
+  path: string;
+  localRelativePath: string;
+}
+
+let SamplesByKey: SamplesByKey = {};
+let Samples: Sample[] = [];
+
 context.keys().forEach(function(key) {
-  debugger;
-  allSamples[key] = context(key);
+  const sample: Sample = {
+    filename: key.split("/").pop() || "KEY_SPLIT_ERROR",
+    path: context(key),
+    localRelativePath: key
+  };
+  SamplesByKey[sample.filename.split(".")[0]] = sample;
+  Samples.push(sample);
 });
 
-export default allSamples;
+export default Samples;
+export { SamplesByKey };
